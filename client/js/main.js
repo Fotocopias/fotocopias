b68659be753b7fc200d9159774974a37d596b097
antoniosf597 <antoniosf597@gmail.com>
2017-04-23 20:08:48 +0200

    Intento de subir un archivo

--- client/js/main.js
+++ client/js/main.js
@@ -4,30 +4,49 @@ $(document).ready(function(){
 	 * En el sigiente codigo hace una llamada por ajax a la ruta api/Grupos
 	 *  y devuelve todos los datos de los grupos que hay
 	 */
-
 	var route = "api/Grupos";
 	var cadena = "";
 	var cadenaGrupo = "";
+	var tmppath = "";
 	    $.ajax(route, {
         success: function (data) {
-        	// var json = truncate(JSON.stringify(data, null, 2), length);
-        	// $('#result').html(json);
-        	//var object = JSON.stringify(data, null, 2);
-        	//alert(data[0]['grupo']);
         	
-        	for (x in data) {
-
-        		var curso = data[x]["curso"];
+			for (x in data) {
+				var curso = data[x]["curso"];
         		cadena = cadena + '<option value="'+curso+'">'+curso+'º</option>';
 
         		var grupo = data[x]["grupo"];
         		cadenaGrupo = cadenaGrupo + '<option value="'+grupo+'">'+grupo+'</option>';
         	}
-        	 $('#curso').html(cadena);
-        	 $('#grupo').html(cadenaGrupo);
+        	$('#curso').html(cadena);
+        	$('#grupo').html(cadenaGrupo);
         }
-      });
+      	});
 	 
+	$('#btn-subir').click(function(){
+		/*
+		 * AL hacer click en el boton de subir se envian por ajax el nombre del archivo seleccionado
+		 * para que se haga un update en  curso y el grupo seleccionado
+		*/
+		var archivo = $('#archivo').val();
+		var curso = $('#curso').val();
+		var grupo = $('#grupo').val();
+/*  *****************NO ME FUNCIONA ESTA FUNCION ********** */
+		$.ajax({
+			url: "api/Grupos/update",
+			type: "POST",
+		    data: { "where":{"curso": curso, "grupo":grupo}, "data":{"archivo": archivo} },
+			success: function(data) {
+				alert('exito');
+			}
+	     });
 
-	
+		
+
+	});
+	$('#archivo').change(function(event){
+		tmppath = URL.createObjectURL(event.target.files[0]);
+		// Esta es la ruta actal del archivo deleccionado
+
+	});
 }); 
--- common/models/grupo.js
+++ common/models/grupo.js
@@ -1,5 +1,15 @@
 'use strict';
 
 module.exports = function(Grupo) {
+   /*Grupo.movefile = function(url, cb) {
+    	var result = true;
 
+		cb(null, result);
+    }
+
+    Grupo.remoteMethod('movefile', {
+		accepts: {arg: 'url', type: 'string'},
+		returns: {arg: 'result', type: 'boolean'}
+    });
+*/
 };
