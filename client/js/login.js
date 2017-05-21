93cc4a53320bec360293aae6fd72d6a7c8ad267a
ricar14 <ricar.gomez55@gmail.com>
2017-05-21 18:42:38 +0200

    Login Usuario a vista profesor EXITO

--- client/js/login.js
+++ client/js/login.js
@@ -1,34 +1,34 @@
 $(document).ready(function(){
 
-$('#loginSubmit').click(function(){
-        var username = $('#username').val();
-        var password = $('#password').val();
+  $('#loginSubmit').click(function(){
+          var username = $('#username').val();
+          var password = $('#password').val();
 
 
-    $.ajax({  
-        type: "POST",
-        url: "api/Usuarios/login",  // Envia el login
-        data: { "username":username, "password":password },  
-       // success: function(msg){  
-       
+      $.ajax({  
+          method: "POST",
+          url: "api/Usuarios/login",  // Envia el login
+          data: $("#UsrLogin").serialize(),  
+         // success: function(msg){  
+         
       }).done(function(res){
-            if(typeof(res.id) !== undefined){
-                window.location.href = "profesor.html";
-                console.log("Logeo exitoso");
-            } else {
-                console.log("Error");
-      })
-    .fail(function(evt){
-        var msgError = "ERROR: "+evt.status+" "+evt.statusText;
-    });  
-  
-// -- Fin AJAX --
+              if(typeof(res.id) !== undefined){
+                  window.location.href = "profesor.html";
+                  console.log("Logeo exitoso");
+              } else {
+                  console.log("Error");
+              }
+        }).fail(function(evt){
+          var msgError = "ERROR: "+evt.status+" "+evt.statusText;
+          });  
+    
+  // -- Fin AJAX --
 
-//return false;
+  return false;
 
-      });
+  });
 });
-};
+
 /*
 $.ajax({  
         type: "POST",
